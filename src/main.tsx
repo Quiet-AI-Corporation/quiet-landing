8d4311a3487267417a1853307d239e908f17678e
N K <nishant.karandikar@gmail.com>
2026-06-09 22:23:33 -0400

    update thank you page with google tag and make it standalone route (#38)

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import LandingPage from './pages/LandingPage'
 import WorkflowDemoPage from './pages/WorkflowDemoPage'
 import PurchasingDemoPage from './pages/PurchasingDemoPage'
-import ThankYouPage from './pages/ThankYouPage'
 import './index.css'
 
 function readRoute(): string {
@@ -27,7 +26,6 @@ function Root() {
 
   if (route === 'demo') return <WorkflowDemoPage />
   if (route === 'purchasing-demo') return <PurchasingDemoPage />
-  if (route === 'thank-you') return <ThankYouPage />
   return <LandingPage />
 }
 
